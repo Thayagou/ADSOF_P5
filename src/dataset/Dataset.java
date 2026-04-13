58c0eb3431dead3644dba9f7bdc11d1c6a47ac6a
Thayagou <tiagooselka@gmail.com>
2026-04-13 18:17:04 +0200

    Dataset

--- src/dataset/Dataset.java
+++ src/dataset/Dataset.java
@@ -2,9 +2,24 @@ package dataset;
 
 import features.Featurizer;
 
+// TODO: Auto-generated Javadoc
+/**
+ * Tipo: Class Dataset.
+ *
+ * @param <T> parámetro genérico
+ */
 public class Dataset<T>{
+	
+	/** Campo featurizer. */
 	private Featurizer<T> featurizer;
+	
+	
+	/**
+	 * Instancia un nuevo Objeto Dataset.
+	 *
+	 * @param featurizer parámetro featurizer
+	 */
 	public Dataset(Featurizer<T> featurizer) {
-		
+		this.featurizer = featurizer;
 	}
 }
